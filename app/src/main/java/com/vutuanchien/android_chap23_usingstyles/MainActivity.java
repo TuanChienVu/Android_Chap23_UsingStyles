7ad26599c40a80341373bff8a58153aec09e2a08
VuTuanChien <jbtuanchien.11@gmail.com>
2016-03-29 01:33:41 +0700

    Android_Chap23_UsingStyles
    Android_Chap23_UsingStyles

---
+++ app/src/main/java/com/vutuanchien/android_chap23_usingstyles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vutuanchien.android_chap23_usingstyles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
